a5800e9e7f569d2191aa7fed37d95f880ec69602
Anish Tewari <aneeshtiwari24@gmail.com>
2023-01-01 00:33:51 +0530

    add mock data

---
+++ src/utils/utils.js
@@ -0,0 +1,36 @@
+function db(){
+    return 
+        [
+           {
+            name:"Task 1",
+            desc:"Learn React",
+            status:false,
+            id:1
+        },
+        {
+            name:"Task 2",
+            desc:"Learn Redux",
+            status:true,
+            id:2
+        },
+        {
+            name:"Task 3",
+            desc:"Learn Node-js",
+            status:true,
+            id:3
+        },
+        {
+            name:"Task 4",
+            desc:"Learn Dsa",
+            status:true,
+            id:4
+        },
+        {
+            name:"Task 5",
+            desc:"Adding new feature in admin panel",
+            status:false,
+            id:5
+        }
+        ]
+    
+}
